5e324a7e1a50b041797f9f3b8691ca1447623b64
Jason Florentino <jasonmflorentino@gmail.com>
2022-01-25 22:06:32 -0500

    Add used word: knoll

--- shared/words.mjs
+++ shared/words.mjs
@@ -1,4 +1,4 @@
-const usedWordsArray = ["rebus", "boost", "truss", "siege", "tiger", "banal", "slump", "crank", "query", "drink", "favor", "abbey", "tangy", "panic", "solar", "shire", "proxy", "point", "robot", "prick", "wince", "crimp"]
+const usedWordsArray = ["rebus", "boost", "truss", "siege", "tiger", "banal", "slump", "crank", "query", "drink", "favor", "abbey", "tangy", "panic", "solar", "shire", "proxy", "point", "robot", "prick", "wince", "crimp", "knoll"]
 // NOTE: The above line is used in /scripts/addword.sh
 //       Be careful if editing this line directly!
 
