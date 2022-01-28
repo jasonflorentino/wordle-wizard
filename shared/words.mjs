e16cb2486f6467be9bdaf989c1736ce535a178f0
Jason Florentino <jasonmflorentino@gmail.com>
2022-01-28 09:38:04 -0500

    Add used word: mount

--- shared/words.mjs
+++ shared/words.mjs
@@ -1,4 +1,4 @@
-const usedWordsArray = ["rebus", "boost", "truss", "siege", "tiger", "banal", "slump", "crank", "query", "drink", "favor", "abbey", "tangy", "panic", "solar", "shire", "proxy", "point", "robot", "prick", "wince", "crimp", "knoll", "sugar", "whack"]
+const usedWordsArray = ["rebus", "boost", "truss", "siege", "tiger", "banal", "slump", "crank", "query", "drink", "favor", "abbey", "tangy", "panic", "solar", "shire", "proxy", "point", "robot", "prick", "wince", "crimp", "knoll", "sugar", "whack", "mount"]
 // NOTE: The above line is used in /scripts/addword.sh
 //       Be careful if editing this line directly!
 
