0c587d06b6cc5c3e5e34bdb759e1a759dc6ccd04
Jason Florentino <jasonmflorentino@gmail.com>
2022-01-28 09:37:51 -0500

    Add used word: whack

--- shared/words.mjs
+++ shared/words.mjs
@@ -1,4 +1,4 @@
-const usedWordsArray = ["rebus", "boost", "truss", "siege", "tiger", "banal", "slump", "crank", "query", "drink", "favor", "abbey", "tangy", "panic", "solar", "shire", "proxy", "point", "robot", "prick", "wince", "crimp", "knoll", "sugar"]
+const usedWordsArray = ["rebus", "boost", "truss", "siege", "tiger", "banal", "slump", "crank", "query", "drink", "favor", "abbey", "tangy", "panic", "solar", "shire", "proxy", "point", "robot", "prick", "wince", "crimp", "knoll", "sugar", "whack"]
 // NOTE: The above line is used in /scripts/addword.sh
 //       Be careful if editing this line directly!
 
